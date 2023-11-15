13894bc32c458a1aab17289ce71f50da0636a94f
Anay Baid <anaybaid7@gmail.com>
2023-11-15 00:53:00 -0500

    Create mixin.js
    updated

---
+++ mixin.js
@@ -0,0 +1,14 @@
+var mixin = function (newObj, oldObj) {
+	if(typeof newObj !== 'object') {
+		throw 'Please provide an object';
+	}
+	var result = oldObj || {}; // can be empty obj
+	for ( var key in newObj ) {
+		if( newObj.hasOwnProperty(key) ) {
+			result[key] = newObj[key];
+		}
+	}
+	return result;
+};
+
+module.exports = mixin;
